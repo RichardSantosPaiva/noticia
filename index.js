3d13aec8f26dc7892c500686d7acde5283052b94
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-11-24 23:35:20 -0300

    v2

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require('express'); // Corrigido para 'express'
 const app = express();
-const path = require('path');
+const path = require('path'); // Corrigido para 'path'
 const newsRoutes = require('./src/routes/news'); // Importar as rotas
 
 app.set('view engine', 'ejs'); // Configurar EJS como motor de visualização
-app.set('views', path.join(__dirname, 'src/views')); // Definir o diretório das views
+app.set('views', path.join(__dirname, 'src/views')); // Define o diretório das visualizações
 
 app.use(express.static(path.join(__dirname, 'public'))); // Para arquivos estáticos
 
 // Usar as rotas para notícias
-app.use('/', newsRoutes); // As rotas começam aqui
+app.use('/', newsRoutes); // Corrigido para 'newsRoutes'
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const PORTA = process.env.PORT || 3000; // Corrigido para 'PORT'
+app.listen(PORTA, () => {
+  console.log(`Servidor rodando na porta ${PORTA}`); // Corrigido para 'PORTA'
 });
